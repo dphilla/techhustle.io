c2d08f30be60b3eed2cb230e0928f7442ed31eaf
Daniel Phillips <danieledward.phillips@gmail.com>
2017-11-06 15:33:26 -0700

    deleted erroneous add default val

--- db/migrate/20171106221455_add_default_relationship_to_connection.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultRelationshipToConnection < ActiveRecord::Migration[5.1]
-  def change
-    change_column :connections, :relationship_id, :integer, :default => Relationship.first.id
-  end
-end
