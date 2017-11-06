2557c86ea1f9060c1e1d59d5d582354edb5fb2de
Daniel Phillips <danieledward.phillips@gmail.com>
2017-11-06 12:37:33 -0700

    Add CRUD for events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,40 @@
+class EventsController < ApplicationController
+before_action :check_current_user
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    redirect_to events_path
+  end
+
+end
